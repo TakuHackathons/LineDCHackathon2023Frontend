0f93b5a0a0f45a6f0213dd57fcf4c3cc65e2ddf6
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2023-02-25 16:53:25 +0900

    Updates

---
+++ _next/static/bFFfdqKOGtAUID89Kb1R_/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/ab0a8544bba5f17d.css","static/chunks/pages/index-ac4f98133d6b3145.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
