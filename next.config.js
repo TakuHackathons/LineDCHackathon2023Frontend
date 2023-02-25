ddeaac661db8d1533fe78b9b04525fe825551e31
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2023-02-25 16:35:44 +0900

    github pagesにdeployできるような形に引用してきて適用させた

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
+const isDeployGithubPages = process.env.DEPLOY_GITHUB_PAGES != undefined;
+
 const nextConfig = {
   reactStrictMode: true,
-}
+  env: {
+    isDeployGithubPages: isDeployGithubPages,
+  },
+  basePath: isDeployGithubPages ? '/LineDCHackathon2023Frontend' : '',
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
