6d3bee25363d9767e07aefbdda6936afb1875b7a
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2023-02-26 09:15:40 +0900

    axios と tailwindcssを入れた

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+}
